411d0641e2013430df91a503ebf562bde30c6917
Alexandre Gagne <salemalex@hotmail.com>
2020-02-29 19:04:17 -0500

    fix get request errors, add documentation

--- src/main/java/ca/utoronto/utm/mcs/PostService.java
+++ src/main/java/ca/utoronto/utm/mcs/PostService.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.bson.BSON;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
@@ -47,13 +46,11 @@ public class PostService implements HttpHandler {
 	public PostService(MongoClient db) {
 		this.db = db;
 	}
-
+	
 	@Override
 	public void handle(HttpExchange r) throws IOException {
 		
-		
 		try {
-			
 			
 			// PUT /api/v1/post
 			if (r.getRequestMethod().equals("PUT")) {
@@ -61,9 +58,9 @@ public class PostService implements HttpHandler {
 				// Convert HTTP Request Body to JSON Object
 				JSONObject body = new JSONObject(Utils.convert(r.getRequestBody()));
 				
-				// Check if all required attributes provided
+				// Check if given all required attributes
 				if (!body.has("title") || !body.has("author") || !body.has("content") || !body.has("tags")) {
-					r.sendResponseHeaders(400, -1); // 400 BAD REQUEST 
+					r.sendResponseHeaders(400, -1); // 400 BAD REQUEST
                 	return;
 				}
 				
@@ -71,11 +68,9 @@ public class PostService implements HttpHandler {
 				String title = body.getString("title");
                 String author = body.getString("author");
                 String content = body.getString("content");
-                
-                JSONArray temp = body.getJSONArray("tags");
 				ArrayList<String> tags = new ArrayList<String>();
-                for (int i = 0; i < temp.length(); i++) {
-                	tags.add(temp.getString(i));
+                for (int i = 0; i < body.getJSONArray("tags").length(); i++) {
+                	tags.add(body.getJSONArray("tags").getString(i));
                 }
                 
                 // Create new post with given attributes
@@ -84,13 +79,13 @@ public class PostService implements HttpHandler {
 				post.put("author", author);
 				post.put("content", content);
                 post.put("tags", tags);
-				
-				// Insert Document to Collection
+
+				// Insert new post into database
 				db.getDatabase("csc301a2").getCollection("posts").insertOne(post);
 				
-				// Write JSON Response with appropriate Status Code
+				// Send response with _id of inserted post
 				String response = new JSONObject().put("_id", post.getObjectId("_id")).toString();
-				r.sendResponseHeaders(200, response.length()); // 200 OK
+				r.sendResponseHeaders(200, response.getBytes().length); // 200 OK
 				OutputStream output = r.getResponseBody();
                 output.write(response.getBytes());
                 output.close();
@@ -104,56 +99,70 @@ public class PostService implements HttpHandler {
 				// Convert HTTP Request Body to JSON Object
 				JSONObject body = new JSONObject(Utils.convert(r.getRequestBody()));
 				
-                // Search by _id
+                // If _id is specified (priority)
                 if (body.has("_id")) {
                 	
+                	// Search by _id
                 	String id = body.getString("_id");
+                	
+                	// Check if valid ObjectId
+                	if (!ObjectId.isValid(id)) {
+                		r.sendResponseHeaders(400, -1); // 400 BAD REQUEST
+                    	return;
+                	}
+                	
     				Document query = new Document("_id",  new ObjectId(id));
-    				Document post = db.getDatabase("csc301a2").getCollection("posts").find(query).first();
+    				Document result = db.getDatabase("csc301a2").getCollection("posts").find(query).first();
 
-    				if (post != null) {
+    				// Check if post found by _id
+    				if (result != null) {
     					
-        				String response = post.toJson().toString();
-        				r.sendResponseHeaders(200, response.length()); // 200 OK
+    					// Send response with post content of post found by _id
+    					String post = result.toJson();
+        				String response = post.toString();
+        				r.sendResponseHeaders(200, response.getBytes().length); // 200 OK
         				OutputStream output = r.getResponseBody();
                         output.write(response.getBytes());
                         output.close();
         				return;
     				}
     				
+    				// If post not found by _id
     				r.sendResponseHeaders(404, -1); // 404 NOT FOUND
 					return;
                 }
                 
-                
-                // Search by title
-                if (body.has("title")) {
+                // If title is specified
+                else if (body.has("title")) {
                 	
+                	// Search by title
                 	String title = body.getString("title");
                 	Bson query = Filters.regex("title", title);
-    				MongoCursor<Document> posts = db.getDatabase("csc301a2").getCollection("posts").find(query).cursor();
+    				MongoCursor<Document> result = db.getDatabase("csc301a2").getCollection("posts").find(query).cursor();
 		
-    				
-    				if (!posts.hasNext()) {
-    					r.sendResponseHeaders(404, -1); // 404 NOT FOUND 
-                    	return;
+    				// Check if post found by title
+    				if (result.hasNext()) {
+
+    					ArrayList<String> posts = new ArrayList<String>();
+        				while (result.hasNext()) {
+        					posts.add(result.next().toJson());
+        				}
+
+        				// Send response with post content of post(s) found by title
+        				String response = posts.toString();
+        				r.sendResponseHeaders(200, response.getBytes().length); // 200 OK
+        				OutputStream output = r.getResponseBody();
+                        output.write(response.getBytes());
+                        output.close();
+        				return;
     				}
-    				
-    				ArrayList<String> temp = new ArrayList<String>();
-    				while (posts.hasNext()) {
-    					temp.add(posts.next().toJson());
-    				}
-    				
-    				String response = temp.toString();
-    				r.sendResponseHeaders(200, response.length()); // 200 OK
-    				OutputStream output = r.getResponseBody();
-                    output.write(response.getBytes());
-                    output.close();
-    				return;
+
+    				// If post(s) not found by title
+    				r.sendResponseHeaders(404, -1); // 404 NOT FOUND 
+                	return;
                 }
-                
-                
-                // If no _id and no title
+
+                // If no _id and no title specified
                 else {
                 	r.sendResponseHeaders(400, -1); // 400 BAD REQUEST 
                 	return;
@@ -166,25 +175,27 @@ public class PostService implements HttpHandler {
 
 				// Convert HTTP Request Body to JSON Object
 				JSONObject body = new JSONObject(Utils.convert(r.getRequestBody()));
-				              
+
+				// If _id not specified
                 if (!body.has("_id")) {
                 	r.sendResponseHeaders(400, -1); // 400 BAD REQUEST 
                 	return;
                 }
-                
+    
+                // Search by _id
                 String id = body.getString("_id");
 				Document query = new Document("_id",  new ObjectId(id));
-			
-				if (db.getDatabase("csc301a2").getCollection("posts").find(query).first() != null) {
-					db.getDatabase("csc301a2").getCollection("posts").findOneAndDelete(query);
+		
+				// Delete post if found by _id
+				if (db.getDatabase("csc301a2").getCollection("posts").findOneAndDelete(query) != null) {
 					r.sendResponseHeaders(200, -1); // 200 OK
 					return;
 				}
 
+				// If post not found by _id
 				r.sendResponseHeaders(404, -1); // 404 NOT FOUND
 				return;
 			}
-			
 			
 			// If method other than PUT, GET or DELETE
 			else {
